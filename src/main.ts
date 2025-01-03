997b5a9a0f8b475cd1b102f43a3bfe315a4ae0fa
Amodh Dhakal <amodhakal@gmail.com>
2025-01-03 13:31:54 -0500

    Refactored HTML canvas dimensions to use TS constants

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { BG_COLOR } from "./constants";
+import { BG_COLOR, BOARD_DIMENSION, BOARD_VIEW_MULTIPLYER } from "./constants";
 import "./style.css";
 
 // Create WebGL context and add a green background
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+canvas.width = BOARD_DIMENSION.width * BOARD_VIEW_MULTIPLYER;
+canvas.height = BOARD_DIMENSION.height * BOARD_VIEW_MULTIPLYER;
 const gl = canvas.getContext("webgl");
 if (!gl) throw new Error("Couldn't create webgl context");
 gl.clearColor(...BG_COLOR);
